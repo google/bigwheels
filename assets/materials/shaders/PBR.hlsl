954889fc0add2afebc6f3e8a5fd06c039dd9f0d7
Hai Nguyen <codingforlove@gmail.com>
2023-04-18 11:09:05 -0400

    Fixed indention and other whitespace issues

--- assets/materials/shaders/PBR.hlsl
+++ assets/materials/shaders/PBR.hlsl
@@ -34,19 +34,19 @@ float Distribution_GGX(float3 N, float3 H, float roughness)
     float NoH2   = NoH * NoH;
     float alpha2 = max(roughness * roughness, EPSILON);
     float A      = NoH2 * (alpha2 - 1) + 1;
-	return alpha2 / (PI * A * A);
+    return alpha2 / (PI * A * A);
 }
 
 float Geometry_SchlickBeckman(float NoV, float k)
 {
-	return NoV / (NoV * (1 - k) + k);
+    return NoV / (NoV * (1 - k) + k);
 }
 
 float Geometry_Smiths(float3 N, float3 V, float3 L,  float roughness)
-{    
-    float k   = pow(roughness + 1, 2) / 8.0; 
+{
+    float k   = pow(roughness + 1, 2) / 8.0;
     float NoL = saturate(dot(N, L));
-    float NoV = saturate(dot(N, V));    
+    float NoV = saturate(dot(N, V));
     float G1  = Geometry_SchlickBeckman(NoV, k);
     float G2  = Geometry_SchlickBeckman(NoL, k);
     return G1 * G2;
@@ -58,7 +58,7 @@ float3 Fresnel_SchlickRoughness(float cosTheta, float3 F0, float roughness)
     return F0 + (max(r, F0) - F0) * pow(1 - cosTheta, 5);
 }
 
-float Lambert() 
+float Lambert()
 {
     return 1.0 / PI;
 }
@@ -67,34 +67,34 @@ float3 Environment(Texture2D tex, float3 coord, float lod)
 {
     float2 uv = CartesianToSphereical(normalize(coord));
     uv.x = saturate(uv.x / (2.0 * PI));
-    uv.y = saturate(uv.y / PI);   
+    uv.y = saturate(uv.y / PI);
     float3 color = tex.SampleLevel(ClampedSampler, uv, lod).rgb;
     return color;
 }
 
 float4 psmain(VSOutput input) : SV_TARGET
 {
-    float3 P = input.positionWS;    
-    float3 N = normalize(input.normal);  
+    float3 P = input.positionWS;
+    float3 N = normalize(input.normal);
     float3 V = normalize(Scene.eyePosition.xyz - input.positionWS);
-    
+
     float3 albedo = Material.albedo;
     if (Material.albedoSelect == 1) {
         albedo = AlbedoTex.Sample(ClampedSampler, input.texCoord).rgb;
         // Remove gamma
-        albedo = pow(albedo, 2.2);        
+        albedo = pow(albedo, 2.2);
     }
-    
+
     float roughness = Material.roughness;
     if (Material.roughnessSelect == 1) {
         roughness = RoughnessTex.Sample(ClampedSampler, input.texCoord).r;
     }
-    
+
     float metalness = Material.metalness;
     if (Material.metalnessSelect == 1) {
         metalness = MetalnessTex.Sample(ClampedSampler, input.texCoord).r;
     }
-   
+
     if (Material.normalSelect == 1) {
         float3   nTS = normalize(input.normalTS);
         float3   tTS = normalize(input.tangentTS);
@@ -102,13 +102,13 @@ float4 psmain(VSOutput input) : SV_TARGET
         float3x3 TBN = float3x3(tTS.x, bTS.x, nTS.x,
                                 tTS.y, bTS.y, nTS.y,
                                 tTS.z, bTS.z, nTS.z);
-        
+
         // Read normal map value in tangent space
         float3 normal = (NormalMapTex.Sample(ClampedSampler, input.texCoord).rgb * 2.0) - 1.0;
 
         // Transform from tangent space to world space
         N = mul(TBN, normal);
-        
+
         // Normalize the transformed normal
         N = normalize(N);
     }
@@ -119,15 +119,15 @@ float4 psmain(VSOutput input) : SV_TARGET
 
     // Calculate F0
     float  specularReflectance = 0.5;
-    float3 F0 = 0.16 * specularReflectance * specularReflectance * (1 - metalness) + albedo * metalness;    
+    float3 F0 = 0.16 * specularReflectance * specularReflectance * (1 - metalness) + albedo * metalness;
 
-    // Reflection and cosine angle bewteen N and V 
+    // Reflection and cosine angle bewteen N and V
     float3 R = reflect(-V, N);
     float  NoV = saturate(dot(N, V));
 
     // Calculate direct lighting
     float3 directLighting = 0;
-	for (uint i = 0; i < Scene.lightCount; ++i) {    
+    for (uint i = 0; i < Scene.lightCount; ++i) {
         // Light variables - world space
         float3 L   = normalize(Lights[i].position - P);
         float3 H   = normalize(L + V);
@@ -137,17 +137,17 @@ float4 psmain(VSOutput input) : SV_TARGET
         float  NoL = saturate(dot(N, L));
 
         // Specular BRDF
-        float  cosTheta = saturate(dot(H, V));        
+        float  cosTheta = saturate(dot(H, V));
         float  D = Distribution_GGX(N, H, roughness);
         float3 F = Fresnel_SchlickRoughness(cosTheta, F0, roughness);
         float  G = Geometry_Smiths(N, V, L, roughness);
-        float  Vis = G / max(0.0001, (4.0 * NoV * NoL));       
+        float  Vis = G / max(0.0001, (4.0 * NoV * NoL));
         float3 specular = (D * F * Vis);
         float3 dielectricSpecular = (1.0 - metalness) * specularReflectance * specular; // non-metal specular
         float3 metalSpecular = metalness * specular;
         float3 specularBRDF = dielectricSpecular + metalSpecular;
 
-        // Fresnel refelectance for diffuse 
+        // Fresnel refelectance for diffuse
         float3 kD = (1.0 - F) * (1.0 - metalness);
 
         // Diffuse BRDF
@@ -167,7 +167,7 @@ float4 psmain(VSOutput input) : SV_TARGET
     float3 finalColor = directLighting + indirectLighting;
 
     // Reapply gamma
-    finalColor = pow(finalColor, 1 / 2.2);    
+    finalColor = pow(finalColor, 1 / 2.2);
 
-    return float4(finalColor, 1);  
+    return float4(finalColor, 1);
 }
