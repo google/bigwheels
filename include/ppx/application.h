2dba5ac873c2cbd1817e8653d8c2026b1ae8898f
Drew Thomas <async@google.com>
2023-04-24 14:37:56 -0700

    Removing command line flag.

--- include/ppx/application.h
+++ include/ppx/application.h
@@ -490,10 +490,9 @@ private:
 
 #if defined(PPX_BUILD_XR)
     XrComponent mXrComponent;
-    uint32_t    mDebugCaptureSwapchainIndex  = 0;
-    uint32_t    mUISwapchainIndex            = 0;
-    uint32_t    mStereoscopicSwapchainIndex  = 0;
-    int         mTogglePassthroughFrameCount = 0;
+    uint32_t    mDebugCaptureSwapchainIndex = 0;
+    uint32_t    mUISwapchainIndex           = 0;
+    uint32_t    mStereoscopicSwapchainIndex = 0;
 #endif
 };
 
