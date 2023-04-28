487743a797781eb4bb904eb14576d0acaf5829a2
Peter Chen <tianc@google.com>
2023-04-28 14:14:21 -0400

    Event loop implementation for Android. (#132)
    * Actually have a proper Android event loop
    * Use Android screen size, instead of hardcoded 720p
    * ImGui input

--- src/android/main.cpp
+++ src/android/main.cpp
@@ -4,10 +4,7 @@
 #include <vector>
 #include <android_native_app_glue.h>
 
-extern bool InitVulkan(android_app* app);
-extern void DeleteVulkan(void);
-extern bool IsVulkanReady(void);
-extern bool VulkanDrawFrame(int argc, char** argv);
+extern bool RunApp(android_app* pAndroidContext, int argc, char** argv);
 
 /*!
  * Call the Java method constructCmdLineArgs on the MainActivity class to get
@@ -42,55 +39,17 @@ static std::vector<std::string> get_java_args(android_app* pApp)
 extern "C"
 {
     /*!
-     * Handles commands sent to this Android application
-     * @param pApp the app the commands are coming from
-     * @param cmd the command to handle
-     */
-    void handle_cmd(android_app* pApp, int32_t cmd)
-    {
-        switch (cmd) {
-            case APP_CMD_INIT_WINDOW:
-                InitVulkan(pApp);
-                break;
-            case APP_CMD_TERM_WINDOW:
-                // The window is being destroyed. Use this to clean up your userData to avoid leaking
-                // resources.
-                DeleteVulkan();
-                break;
-            default:
-                break;
-        }
-    }
-
-    /*!
      * This the main entry point for a native activity
      */
     void android_main(struct android_app* pApp)
     {
-        // register an event handler for Android events
-        pApp->onAppCmd = handle_cmd;
+        std::vector<std::string> cmdArgs = get_java_args(pApp);
 
-        // This sets up a typical game/event loop. It will run until the app is destroyed.
-        int                  events;
-        android_poll_source* pSource;
-        do {
-            // Process all pending events before running game logic.
-            if (ALooper_pollAll(0, nullptr, &events, (void**)&pSource) >= 0) {
-                if (pSource) {
-                    pSource->process(pApp, pSource);
-                }
-            }
-
-            if (IsVulkanReady()) {
-                std::vector<std::string> cmdArgs = get_java_args(pApp);
-
-                std::vector<char*> args;
-                args.reserve(cmdArgs.size());
-                for (std::string& arg : cmdArgs) {
-                    args.push_back(const_cast<char*>(arg.c_str()));
-                }
-                VulkanDrawFrame(cmdArgs.size(), args.data());
-            }
-        } while (!pApp->destroyRequested);
+        std::vector<char*> args;
+        args.reserve(cmdArgs.size());
+        for (std::string& arg : cmdArgs) {
+            args.push_back(const_cast<char*>(arg.c_str()));
+        }
+        RunApp(pApp, cmdArgs.size(), args.data());
     }
 }
