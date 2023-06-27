3d5ecec65f56395db7191ffa4205fa8dfb811fc5
Angela Chen <chenangela@google.com>
2023-06-27 14:33:42 -0400

    Reorder initializer list for KnobCheckbox

--- src/ppx/knob.cpp
+++ src/ppx/knob.cpp
@@ -36,7 +36,7 @@ bool Knob::DigestUpdate()
 // -------------------------------------------------------------------------------------------------
 
 KnobCheckbox::KnobCheckbox(const std::string& flagName, bool defaultValue)
-    : Knob(flagName), mDefaultValue(defaultValue), mValue(defaultValue)
+    : Knob(flagName), mValue(defaultValue), mDefaultValue(defaultValue)
 {
     SetFlagParameters("<true|false>");
     RaiseUpdatedFlag();
