157e8a1b686f63247ea7abc46ab5b174a2226585
Elvis Capia <ecapia@google.com>
2023-07-31 13:26:09 -0400

    Convert tabs to spaces in `Benchmark_SkyBox.hlsl`

--- assets/benchmarks/shaders/Benchmark_SkyBox.hlsl
+++ assets/benchmarks/shaders/Benchmark_SkyBox.hlsl
@@ -22,19 +22,19 @@ Texture2D                     Tex0      : register(t1);
 SamplerState                  Sampler0  : register(s2);
 
 struct VSOutput {
-	float4 Position : SV_POSITION;
-	float2 TexCoord : TEXCOORD;
+    float4 Position : SV_POSITION;
+    float2 TexCoord : TEXCOORD;
 };
 
 VSOutput vsmain(float4 Position : POSITION, float2 TexCoord : TEXCOORD0)
 {
-	VSOutput result;
-	result.Position = mul(Transform.MVP, Position);
-	result.TexCoord = TexCoord;
-	return result;
+    VSOutput result;
+    result.Position = mul(Transform.MVP, Position);
+    result.TexCoord = TexCoord;
+    return result;
 }
 
 float4 psmain(VSOutput Input) : SV_TARGET
 {
-	return Tex0.Sample(Sampler0, Input.TexCoord);
+    return Tex0.Sample(Sampler0, Input.TexCoord);
 }
