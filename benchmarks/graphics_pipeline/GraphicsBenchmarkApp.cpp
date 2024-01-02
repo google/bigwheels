11b116718dffa565c9a38a467ec22f47a59ea3cd
Angela Chen <chenangela@google.com>
2024-01-02 11:54:46 -0500

    Removed brackets for break statement

--- benchmarks/graphics_pipeline/GraphicsBenchmarkApp.cpp
+++ benchmarks/graphics_pipeline/GraphicsBenchmarkApp.cpp
@@ -1696,9 +1696,8 @@ void GraphicsBenchmarkApp::RecordCommandBufferFullscreenQuad(PerFrame& frame, si
             frame.cmd->PushGraphicsConstants(mQuadsPipelineInterfaces[1], 3, &colorValues);
             break;
         }
-        case FullscreenQuadsType::FULLSCREEN_QUADS_TYPE_TEXTURE: {
+        case FullscreenQuadsType::FULLSCREEN_QUADS_TYPE_TEXTURE:
             break;
-        }
         default: {
             PPX_ASSERT_MSG(true, "unsupported FullscreenQuadsType: " << static_cast<int>(pFullscreenQuadsType->GetValue()));
         }
