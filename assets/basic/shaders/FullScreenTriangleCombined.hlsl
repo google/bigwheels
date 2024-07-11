3ecccaccce3bc999b6b53fd42b5ed23c103720f6
Jorge Garcia Galicia <jorgeag@google.com>
2024-07-11 16:19:15 -0700

    Adress simple comments

--- assets/basic/shaders/FullScreenTriangleCombined.hlsl
+++ assets/basic/shaders/FullScreenTriangleCombined.hlsl
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,28 +22,28 @@ Texture2D    Tex0     : register(t0);
 SamplerState Sampler0 : register(s0);
 
 struct VSOutput {
-	float4 Position : SV_POSITION;
-	float2 TexCoord : TEXCOORD;
+    float4 Position : SV_POSITION;
+    float2 TexCoord : TEXCOORD;
 };
 
 VSOutput vsmain(uint id : SV_VertexID)
 {
-	VSOutput result;
+    VSOutput result;
 
     // Clip space position
-	result.Position.x = (float)(id / 2) * 4.0 - 1.0;
+    result.Position.x = (float)(id / 2) * 4.0 - 1.0;
     result.Position.y = (float)(id % 2) * 4.0 - 1.0;
     result.Position.z = 0.0;
     result.Position.w = 1.0;
 
     // Texture coordinates
-	result.TexCoord.x = (float)(id / 2) * 2.0;
+    result.TexCoord.x = (float)(id / 2) * 2.0;
     result.TexCoord.y = 1.0 - (float)(id % 2) * 2.0;
 
-	return result;
+    return result;
 }
 
 float4 psmain(VSOutput input) : SV_TARGET
 {
-	return Tex0.Sample(Sampler0, input.TexCoord);
+    return Tex0.Sample(Sampler0, input.TexCoord);
 }
