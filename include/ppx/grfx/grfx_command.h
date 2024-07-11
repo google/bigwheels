954240918fa594c334911095ff74076400b54a39
Jorge Garcia Galicia <82472990+jorgeag-google@users.noreply.github.com>
2024-07-11 11:56:30 -0700

    Add BlitImage (Vulkan Only) (#487)
    The second of a series of CL that integrates the commits from:
    
    https://github.com/bjoeris/bigwheels/tree/experimental-foveation
    
    into BW main's branch.
    
    The series will have 4 CL, this is the second one that integrates the
    following commit
    
    Add BlitImage (Vulkan Only)
    
    ---------
    
    Co-authored-by: Benson Joeris <bjoeris@google.com>
    Co-authored-by: wangra-google <62302956+wangra-google@users.noreply.github.com>

--- include/ppx/grfx/grfx_command.h
+++ include/ppx/grfx/grfx_command.h
@@ -162,6 +162,28 @@ struct ImageToImageCopyInfo
     } extent;
 };
 
+// struct ImageBlitInfo
+struct ImageBlitInfo
+{
+    struct ImgInfo
+    {
+        uint32_t mipLevel        = 0;
+        uint32_t arrayLayer      = 0; // Must be 0 for 3D images
+        uint32_t arrayLayerCount = 1; // Must be 1 for 3D images
+        struct
+        {
+            uint32_t x = 0; // [pixels]
+            uint32_t y = 0; // [pixels]
+            uint32_t z = 0; // [pixels]
+        } offsets[2];
+    };
+
+    ImgInfo srcImage;
+    ImgInfo dstImage;
+
+    Filter filter = FILTER_LINEAR;
+};
+
 // -------------------------------------------------------------------------------------------------
 
 struct RenderPassBeginInfo
@@ -497,6 +519,11 @@ public:
         const grfx::ImageToImageCopyInfo* pCopyInfo,
         grfx::Image*                      pSrcImage,
         grfx::Image*                      pDstImage) = 0;
+
+    virtual void BlitImage(
+        const grfx::ImageBlitInfo* pCopyInfo,
+        grfx::Image*               pSrcImage,
+        grfx::Image*               pDstImage) = 0;
 
     virtual void BeginQuery(
         const grfx::Query* pQuery,
