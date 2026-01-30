d88e7dc7411474f0f05b6faecf71b6d940d81f8a
Michael Hitchens <hitchens@google.com>
2026-01-30 17:47:48 -0500

    [glTF] Decode image URIs before loading
    glTF 2.0 allows URIs to be percent encoded (section 2.8). To wit: the
    "Box with Spaces" scene from glTF-Sample-Assets defines these images:
    
    ```
        "images" : [
            {
                "mimeType" : "image/png",
                "name" : "Normal Map",
                "uri" : "Normal%20Map.png"
            },
            {
                "mimeType" : "image/png",
                "name" : "glTF Logo With Spaces",
                "uri" : "glTF%20Logo%20With%20Spaces.png"
            },
            {
                "mimeType" : "image/png",
                "name" : "Roughness Metallic",
                "uri" : "Roughness%20Metallic.png"
            }
        ],
    ```
    
    Taken literally, GltfLoader tried to find a file called
    `Normal%20Map.png`. This files doesn't exist so loading failed.
    
    cgltf provides the `cgltf_decode_uri` helper function to remove the
    percent encoding. With this, GltfLoader now tries to load
    `Normal Map.png` instead and succeeds.

--- src/ppx/scene/scene_gltf_loader.cpp
+++ src/ppx/scene/scene_gltf_loader.cpp
@@ -518,6 +518,20 @@ ppx::Result GltfLoader::Create(
         }
     }
 
+    // Relative paths in image URIs may be percent encoded. Since we only expect relative paths,
+    // fix them all up in one go and remove this burden from future users.
+    for (size_t i = 0; i < pGltfData->images_count; ++i) {
+        cgltf_image& image = pGltfData->images[i];
+        if (strncmp(image.uri, "data:", 5) == 0) {
+            PPX_ASSERT_MSG(false, "GLTF: images with data URIs are not supported");
+            return ppx::ERROR_SCENE_INVALID_SOURCE_IMAGE;
+        }
+        // cgltf_decode_uri wants a mutable C-string. Fortunately, image.uri is one! Since
+        // decoding replaces larger substrings with smaller ones, we should be able to safely reuse
+        // the existing buffer. This data is owned by cgltf and will be freed by cgltf_free.
+        cgltf_decode_uri(image.uri);
+    }
+
     // Loading from file means we own the GLTF data
     const bool ownsGltfData = true;
 
